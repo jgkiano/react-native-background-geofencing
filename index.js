18d4d09df3b0ffa783127afb0e3ff92815386ecf
Julius Kiano <jgkiano@gmail.com>
2020-03-28 11:28:06 +0300

    define function to register a client  onGeofenceEventCallback

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import { NativeModules } from 'react-native';
+import {AppRegistry} from 'react-native';
 
 const { BackgroundGeofencing } = NativeModules;
+
+export const onGeofenceEvent = onGeofenceEventCallback => {
+  if (typeof onGeofenceEventCallback === "function") {
+    AppRegistry.registerHeadlessTask('OnGeoFenceEventJavaScript', () => onGeofenceEventCallback);
+  }
+}
 
 export default BackgroundGeofencing;
