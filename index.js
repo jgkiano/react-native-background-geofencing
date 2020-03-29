b226af473101d93fedbe79b147c0a3ba51e9f427
Julius Kiano <jgkiano@gmail.com>
2020-03-29 15:49:26 +0300

    refined BackgroundGeofencing to use defaults first

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-import { NativeModules } from 'react-native';
+import {NativeModules} from 'react-native';
 import {AppRegistry} from 'react-native';
 
-const { BackgroundGeofencing } = NativeModules;
+const {BackgroundGeofencing} = NativeModules;
 
 export const onGeofenceEvent = onGeofenceEventCallback => {
-  if (typeof onGeofenceEventCallback === "function") {
-    AppRegistry.registerHeadlessTask('OnGeoFenceEventJavaScript', () => onGeofenceEventCallback);
+  if (typeof onGeofenceEventCallback === 'function') {
+    AppRegistry.registerHeadlessTask(
+      'OnGeoFenceEventJavaScript',
+      () => onGeofenceEventCallback,
+    );
   }
-}
+};
 
 export default {
-  add(geofence = {}) {
-    const { id, lat, lng } = geofence;
+  async add(geofence = {}) {
+    const {id, lat, lng} = geofence;
     const defaults = {
       radius: 150,
       expiration: -1,
@@ -21,9 +24,15 @@ export default {
       initialiseOnDeviceRestart: false,
       setInitialTriggers: true,
     };
-    if (!id || !lat || !lng) {
-      throw new Error("RN:BackgroundGeofencing:", "Must provide atlease a valid id, lat and lng");
+    try {
+      if (!id || !lat || !lng) {
+        throw new Error(
+          'RN:BackgroundGeofencing: Must provide atlease a valid id, lat and lng',
+        );
+      }
+      return await BackgroundGeofencing.add({...defaults, ...geofence});
+    } catch (error) {
+      throw error;
     }
-    return BackgroundGeofencing.add({ ...defaults, ...geofence});
-  }
-}
+  },
+};
