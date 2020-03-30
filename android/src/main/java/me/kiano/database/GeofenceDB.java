ad1d72e47cb74a907c2dbca40f960098de73b7d8
Julius Kiano <jgkiano@gmail.com>
2020-03-30 10:11:16 +0300

    testing snappydb

--- android/src/main/java/me/kiano/database/GeofenceDB.java
+++ android/src/main/java/me/kiano/database/GeofenceDB.java
@@ -1,37 +1,44 @@
 package me.kiano.database;
 
 import android.content.Context;
+import android.os.Build;
 import android.util.Log;
 
-import com.orhanobut.hawk.Hawk;
+import androidx.annotation.RequiresApi;
 
+import com.snappydb.DB;
+import com.snappydb.DBFactory;
+import com.snappydb.SnappydbException;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 
 public class GeofenceDB {
+    private final Context ctx;
+
     public GeofenceDB(Context context) {
-        Hawk.init(context).build();
+        ctx = context;
     }
 
-    private String STORAGE_KEY = "@savedGeofences";
+    private String DB_NAME = "RNBackgroundGeofencingDB";
 
     private String TAG = "GeofenceDB";
 
     public void saveGeofence (HashMap<String, Object> geofence) {
         try {
+            DB db = DBFactory.open(ctx, DB_NAME);
             ArrayList<HashMap<String, Object>> savedGeofences = new ArrayList<>();
-            savedGeofences = Hawk.get(STORAGE_KEY, savedGeofences);
-            Iterator<HashMap<String, Object>> iterator = savedGeofences.iterator();
-            while (iterator.hasNext()) {
-                HashMap<String, Object> savedGeofence = iterator.next();
-                if (savedGeofence.get("id") == geofence.get("id")) {
-                    savedGeofences.remove(savedGeofence);
-                }
-            }
             savedGeofences.add(geofence);
-            Hawk.put(STORAGE_KEY, savedGeofences);
-            Log.v(TAG, "Successfully saved geofence to DB!");
+            db.put("savedGeofences", savedGeofences);
+            Log.v(TAG, "Added Geofence with id" + geofence.get("id"));
+            ArrayList[] retrievedGeofences = db.getObjectArray("savedGeofences", ArrayList.class);
+            Log.v(TAG, "Got Geofences" + retrievedGeofences.toString());
+            db.close();
         } catch (Exception e) {
             Log.e(TAG, e.getMessage());
         }
