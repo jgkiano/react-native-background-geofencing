a211d83f0585e10a81dce18c5f7f3c18ce5609b9
Julius Kiano <jgkiano@gmail.com>
2020-03-30 20:40:02 +0300

    store individual geofences as keys

--- android/src/main/java/me/kiano/database/GeofenceDB.java
+++ android/src/main/java/me/kiano/database/GeofenceDB.java
@@ -6,11 +6,6 @@ import android.util.Log;
 import com.snappydb.DB;
 import com.snappydb.DBFactory;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-
 import me.kiano.models.RNGeofence;
 
 public class GeofenceDB {
@@ -22,33 +17,16 @@ public class GeofenceDB {
 
     private String DB_NAME = "RNBackgroundGeofencingDB";
 
-    private String DB_KEY_NAME = "RNGeofences";
+    private String TAG = "RNGeofenceDB";
 
-    private String TAG = "GeofenceDB";
+    private String KEY_PREFIX = "RNGeofenceDB:v1:";
 
     public void saveGeofence (RNGeofence rnGeofence) {
         try {
             DB db = DBFactory.open(context, DB_NAME);
-            List<RNGeofence> rnGeofences = new ArrayList<>();
-            if(db.exists(DB_KEY_NAME)) {
-                ArrayList<RNGeofence> savedRNGeofences = (ArrayList<RNGeofence>) db.getObject(DB_KEY_NAME, ArrayList.class);
-                Log.v(TAG, "DB has values");
-                Iterator iterator = savedRNGeofences.iterator();
-                while (iterator.hasNext()) {
-                    RNGeofence currentRNGeofence = (RNGeofence) iterator.next();
-                    if (currentRNGeofence.id == rnGeofence.id) {
-                        Log.v(TAG, "DB already has geopoint with existing id: " + rnGeofence.id);
-                        savedRNGeofences.remove(currentRNGeofence);
-                    }
-                }
-                savedRNGeofences.add(rnGeofence);
-                rnGeofences = savedRNGeofences;
-            } else {
-                rnGeofences.add(rnGeofence);
-            }
-            Log.v(TAG, "DB new size: " + rnGeofences.size());
-            db.put(DB_KEY_NAME, rnGeofences);
+            db.put(KEY_PREFIX + rnGeofence.id, rnGeofence);
             db.close();
+            Log.v(TAG, "Geofence successfully saved to DB: " + rnGeofence);
         } catch (Exception e) {
             Log.e(TAG, e.getMessage());
         }
