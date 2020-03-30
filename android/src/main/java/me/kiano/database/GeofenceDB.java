c4f654d514df279033256265901629b26b48e82b
Julius Kiano <jgkiano@gmail.com>
2020-03-30 19:51:52 +0300

    saveGeofence to use RNGeofence Model

--- android/src/main/java/me/kiano/database/GeofenceDB.java
+++ android/src/main/java/me/kiano/database/GeofenceDB.java
@@ -1,43 +1,53 @@
 package me.kiano.database;
 
 import android.content.Context;
-import android.os.Build;
 import android.util.Log;
-
-import androidx.annotation.RequiresApi;
 
 import com.snappydb.DB;
 import com.snappydb.DBFactory;
-import com.snappydb.SnappydbException;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
+
+import me.kiano.models.RNGeofence;
 
 public class GeofenceDB {
-    private final Context ctx;
+    private final Context context;
 
     public GeofenceDB(Context context) {
-        ctx = context;
+        this.context = context;
     }
 
     private String DB_NAME = "RNBackgroundGeofencingDB";
 
+    private String DB_KEY_NAME = "RNGeofences";
+
     private String TAG = "GeofenceDB";
 
-    public void saveGeofence (HashMap<String, Object> geofence) {
+    public void saveGeofence (RNGeofence rnGeofence) {
         try {
-            DB db = DBFactory.open(ctx, DB_NAME);
-            ArrayList<HashMap<String, Object>> savedGeofences = new ArrayList<>();
-            savedGeofences.add(geofence);
-            db.put("savedGeofences", savedGeofences);
-            Log.v(TAG, "Added Geofence with id" + geofence.get("id"));
-            ArrayList[] retrievedGeofences = db.getObjectArray("savedGeofences", ArrayList.class);
-            Log.v(TAG, "Got Geofences" + retrievedGeofences.toString());
+            DB db = DBFactory.open(context, DB_NAME);
+            List<RNGeofence> rnGeofences = new ArrayList<>();
+            if(db.exists(DB_KEY_NAME)) {
+                ArrayList<RNGeofence> savedRNGeofences = (ArrayList<RNGeofence>) db.getObject(DB_KEY_NAME, ArrayList.class);
+                Log.v(TAG, "DB has values");
+                Iterator iterator = savedRNGeofences.iterator();
+                while (iterator.hasNext()) {
+                    RNGeofence currentRNGeofence = (RNGeofence) iterator.next();
+                    if (currentRNGeofence.id == rnGeofence.id) {
+                        Log.v(TAG, "DB already has geopoint with existing id: " + rnGeofence.id);
+                        savedRNGeofences.remove(currentRNGeofence);
+                    }
+                }
+                savedRNGeofences.add(rnGeofence);
+                rnGeofences = savedRNGeofences;
+            } else {
+                rnGeofences.add(rnGeofence);
+            }
+            Log.v(TAG, "DB new size: " + rnGeofences.size());
+            db.put(DB_KEY_NAME, rnGeofences);
             db.close();
         } catch (Exception e) {
             Log.e(TAG, e.getMessage());
