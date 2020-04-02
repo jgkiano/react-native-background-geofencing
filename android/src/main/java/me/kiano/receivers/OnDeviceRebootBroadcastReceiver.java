66e6eb38afb13ba83963847ac97281102dcfe75e
Julius Kiano <jgkiano@gmail.com>
2020-04-02 15:03:08 +0300

    commented out reinitilisation code

--- android/src/main/java/me/kiano/receivers/OnDeviceRebootBroadcastReceiver.java
+++ android/src/main/java/me/kiano/receivers/OnDeviceRebootBroadcastReceiver.java
@@ -8,19 +8,52 @@ import android.os.Looper;
 import android.util.Log;
 import android.widget.Toast;
 
-import me.kiano.services.ReinitialiseGeofencesJobIntentService;
+import java.util.ArrayList;
+
+import me.kiano.database.GeofenceDB;
+import me.kiano.interfaces.RNGeofenceHandler;
+import me.kiano.models.RNGeofence;
 
 public class OnDeviceRebootBroadcastReceiver extends BroadcastReceiver {
     public static final String TAG = "RNBackgroundGeofencing";
     @Override
     public void onReceive(final Context context, Intent intent) {
         Log.v(TAG, "Device restart detected");
-        Handler handler = new Handler(Looper.getMainLooper());
+
+        final Handler handler = new Handler(Looper.getMainLooper());
+
         handler.post(new Runnable() {
             public void run() {
                 Toast.makeText(context, "We see you've restarted your device -_-", Toast.LENGTH_LONG).show();
             }
         });
-        ReinitialiseGeofencesJobIntentService.enqueueWork(context, intent);
+
+//        Log.v(TAG, "Starting job from OnDeviceRebootBroadcastReceiver  -_-");
+//        GeofenceDB db = new GeofenceDB(context);
+//        ArrayList<RNGeofence> storedGeofences = db.getAllGeofences();
+//        for (RNGeofence storedGeofence: storedGeofences) {
+//            storedGeofence.start(false, new RNGeofenceHandler() {
+//                @Override
+//                public void onSuccess(final String geofenceId) {
+//                    Log.v(TAG, "Geofence successfully reinitialised: " + geofenceId);
+//                    handler.post(new Runnable() {
+//                        public void run() {
+//                            Toast.makeText(context, "Geofence successfully reinitialised: " + geofenceId, Toast.LENGTH_LONG).show();
+//                        }
+//                    });
+//                }
+//                @Override
+//                public void onError(final String geofenceId, Exception e) {
+//                    Log.v(TAG, "Geofence FAILED reinitialisation: " + geofenceId);
+//                    Log.e(TAG, e.getMessage());
+//                    handler.post(new Runnable() {
+//                        public void run() {
+//                            Toast.makeText(context, "Geofence successfully reinitialised: " + geofenceId, Toast.LENGTH_LONG).show();
+//                        }
+//                    });
+//                }
+//            });
+//        }
+
     }
 }
