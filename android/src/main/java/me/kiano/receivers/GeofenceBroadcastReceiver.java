1d787e26740e323294bc1f5fc867603ce37bc814
Julius Kiano <jgkiano@gmail.com>
2020-04-02 15:02:53 +0300

    removed redundant wake lock requirement

--- android/src/main/java/me/kiano/receivers/GeofenceBroadcastReceiver.java
+++ android/src/main/java/me/kiano/receivers/GeofenceBroadcastReceiver.java
@@ -5,10 +5,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import com.facebook.react.HeadlessJsTaskService;
+
 import me.kiano.services.GeofenceTransitionsJobIntentService;
 
 public class GeofenceBroadcastReceiver extends BroadcastReceiver {
     public static final String TAG = "RNBackgroundGeofencing";
+
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.v(TAG, "GeofenceBroadcastReceiver received!");
