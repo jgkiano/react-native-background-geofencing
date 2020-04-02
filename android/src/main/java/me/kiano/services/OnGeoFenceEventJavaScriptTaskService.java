700f0f449c1a964257a9af6c9bd4e96cae868149
Julius Kiano <jgkiano@gmail.com>
2020-04-02 15:20:12 +0300

    added timeout value

--- android/src/main/java/me/kiano/services/OnGeoFenceEventJavaScriptTaskService.java
+++ android/src/main/java/me/kiano/services/OnGeoFenceEventJavaScriptTaskService.java
@@ -1,15 +1,32 @@
 package me.kiano.services;
 
+import android.app.ActivityManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.core.app.NotificationCompat;
+
 import com.facebook.react.HeadlessJsTaskService;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.jstasks.HeadlessJsTaskConfig;
+
+import java.util.List;
+
 import javax.annotation.Nullable;
 
 public class OnGeoFenceEventJavaScriptTaskService extends HeadlessJsTaskService {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
 
     @Override
     protected @Nullable HeadlessJsTaskConfig getTaskConfig(Intent intent) {
@@ -18,7 +35,7 @@ public class OnGeoFenceEventJavaScriptTaskService extends HeadlessJsTaskService 
             return new HeadlessJsTaskConfig(
                     "OnGeoFenceEventJavaScript",
                     Arguments.fromBundle(extras),
-                    5000, // timeout for the task
+                    30000, // timeout for the task
                     true // optional: defines whether or not  the task is allowed in foreground. Default is false
             );
         }
