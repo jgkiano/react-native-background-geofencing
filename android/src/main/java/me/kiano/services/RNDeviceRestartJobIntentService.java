e8db20adbad22c8f9eb298926b97f3fee849643a
Julius Kiano <jgkiano@gmail.com>
2020-04-04 19:19:51 +0300

    created RNDeviceRestartJobIntentService to register geofences on device restarts

---
+++ android/src/main/java/me/kiano/services/RNDeviceRestartJobIntentService.java
@@ -0,0 +1,43 @@
+package me.kiano.services;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.JobIntentService;
+
+import java.util.ArrayList;
+
+import me.kiano.database.RNGeofenceDB;
+import me.kiano.interfaces.RNGeofenceHandler;
+import me.kiano.models.RNGeofence;
+
+public class RNDeviceRestartJobIntentService extends JobIntentService {
+
+    private final static String TAG = "RNDeviceRestartJob";
+
+    public static void enqueueWork(Context context, Intent work) {
+        Log.v(TAG, "RNDeviceRestartJobIntentService called");
+        enqueueWork(context, RNGeofenceTransitionsJobIntentService.class, 456, work);
+    }
+
+    @Override
+    protected void onHandleWork(@NonNull Intent intent) {
+        RNGeofenceDB db = new RNGeofenceDB(getApplication());
+        ArrayList<RNGeofence> storedGeofences = db.getAllGeofences();
+        for (RNGeofence storedGeofence: storedGeofences) {
+            storedGeofence.start(false, new RNGeofenceHandler() {
+                @Override
+                public void onSuccess(final String geofenceId) {
+                    Log.v(TAG, "Geofence successfully reinitialised: " + geofenceId);
+                }
+                @Override
+                public void onError(final String geofenceId, Exception e) {
+                    Log.v(TAG, "Geofence FAILED reinitialisation: " + geofenceId);
+                    Log.e(TAG, e.getMessage());
+                }
+            });
+        }
+    }
+}
