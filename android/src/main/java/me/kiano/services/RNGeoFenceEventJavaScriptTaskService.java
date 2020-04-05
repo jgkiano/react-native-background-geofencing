38539b86c784e03c58b397c32493d4fcf8bcfcba
Julius Kiano <jgkiano@gmail.com>
2020-04-05 16:56:50 +0300

    run headless js task in foreground if app is close

--- android/src/main/java/me/kiano/services/RNGeoFenceEventJavaScriptTaskService.java
+++ android/src/main/java/me/kiano/services/RNGeoFenceEventJavaScriptTaskService.java
@@ -1,19 +1,47 @@
 package me.kiano.services;
 
+import android.app.ActivityManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+
+import androidx.core.app.NotificationCompat;
 
 import com.facebook.react.HeadlessJsTaskService;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.jstasks.HeadlessJsTaskConfig;
 
+import java.util.List;
+
 import javax.annotation.Nullable;
 
 public class RNGeoFenceEventJavaScriptTaskService extends HeadlessJsTaskService {
 
+    private static final String CHANNEL_ID = "RNBackgroundGeofencing";
+
     @Override
     public void onCreate() {
         super.onCreate();
+        if (!isAppOnForeground(getApplicationContext()) && android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "RNBackgroundGeofencing", importance);
+            channel.setDescription("Background geofencing service");
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+
+            Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setContentTitle("Notification title")
+                    .setContentText("Notification title text")
+                    .setSmallIcon(getApplicationContext().getApplicationInfo().icon)
+                    .build();
+
+            startForeground(1, notification);
+        }
     }
 
     @Override
@@ -28,5 +56,23 @@ public class RNGeoFenceEventJavaScriptTaskService extends HeadlessJsTaskService 
             );
         }
         return null;
+    }
+
+    private boolean isAppOnForeground(Context context) {
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> appProcesses =
+                activityManager.getRunningAppProcesses();
+        if (appProcesses == null) {
+            return false;
+        }
+        final String packageName = context.getPackageName();
+        for (ActivityManager.RunningAppProcessInfo appProcess : appProcesses) {
+            if (appProcess.importance ==
+                    ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND &&
+                    appProcess.processName.equals(packageName)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
