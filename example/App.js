4884b7d0cce29440e91384099c6ea5eff49077ab
Julius Kiano <jgkiano@gmail.com>
2020-04-07 10:11:26 +0300

    get permissions

--- example/App.js
+++ example/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Platform} from 'react-native';
+import {request, PERMISSIONS} from 'react-native-permissions';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
@@ -10,7 +12,27 @@ const Stack = createStackNavigator();
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.askPermissions();
   }
+
+  askPermissions = async () => {
+    try {
+      const rational = {
+        title: 'OkHi VaaS needs location permission',
+        message:
+          'We need location permission to enable us to create and verify the addresses you create',
+        buttonPositive: 'GRANT',
+        buttonNegative: 'DENY',
+        buttonNeutral: 'CANCEL',
+      };
+      if (Platform.OS === 'android') {
+        await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION, rational);
+        await request(PERMISSIONS.ANDROID.ACCESS_BACKGROUND_LOCATION, rational);
+      } else {
+        return false;
+      }
+    } catch (error) {}
+  };
 
   render() {
     return (
