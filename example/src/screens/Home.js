b220e2dd82fd1a89262886ddc4c406eef1028898
Julius Kiano <jgkiano@gmail.com>
2020-05-19 20:46:50 +0300

    simplified home screen

--- example/src/screens/Home.js
+++ example/src/screens/Home.js
@@ -1,73 +1,32 @@
-/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {View, Modal} from 'react-native';
+import styled from 'styled-components';
 import Repository from '../services/Repository';
-import FullScreenLoader from '../components/FullScreenLoader';
 import FullButton from '../components/FullButton';
-import HomeGeofences from '../components/HomeGeofences';
 import HomeEmptyState from '../components/HomeEmptyState';
-import ModalBody from '../components/ModalBody';
 
 export default class HomeScreen extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true,
-      geofences: [],
-      modalVisible: false,
-    };
-    this.repo = new Repository();
-    this.timer = setTimeout(async () => {
-      const geofences = await this.repo.getGeofences();
-      this.setState({geofences, loading: false});
-      clearTimeout(this.timer);
-    }, 1500);
-  }
+  repo = new Repository();
 
-  handleFormSubmit = geofence => {
-    this.setState({modalVisible: false, loading: true}, async () => {
-      const geofences = await this.repo.addGeofence(geofence);
-      this.setState({geofences, loading: false});
-    });
-  };
-
-  handleItemTap = geofenceId => {
-    this.props.navigation.push('History', {geofenceId});
+  handleOnPress = () => {
+    const {navigation, route} = this.props;
+    navigation.navigate('AddGeofence', {user: route.params.user});
   };
 
   renderPage = () => {
-    const {loading, geofences} = this.state;
-    if (loading) {
-      return <FullScreenLoader />;
-    }
-    if (geofences && geofences.length) {
-      return (
-        <HomeGeofences geofences={geofences} onItemTap={this.handleItemTap} />
-      );
-    }
     return <HomeEmptyState />;
   };
 
   render() {
-    const {loading, modalVisible} = this.state;
     return (
-      <View style={{flex: 1, flexDirection: 'column'}}>
-        <Modal
-          animationType="slide"
-          transparent={true}
-          visible={modalVisible}
-          onRequestClose={() => {
-            this.setState({modalVisible: false});
-          }}>
-          <ModalBody onSubmit={this.handleFormSubmit} />
-        </Modal>
+      <Container>
         {this.renderPage()}
-        <FullButton
-          label="ADD A GEOFENCE"
-          disabled={loading}
-          onPress={() => this.setState({modalVisible: true})}
-        />
-      </View>
+        <FullButton label="ADD A GEOFENCE" onPress={this.handleOnPress} />
+      </Container>
     );
   }
 }
+
+const Container = styled.View`
+  flex: 1;
+  flex-direction: column;
+`;
