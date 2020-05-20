8453a3f12b2b99be403e4db953515126cb7b5460
Julius Kiano <jgkiano@gmail.com>
2020-05-20 18:40:11 +0300

    added get and save geofences methods

--- example/src/services/Repository.js
+++ example/src/services/Repository.js
@@ -32,32 +32,32 @@ export default class Repository {
 
   addGeofence = async geofence => {
     try {
-      await RNBackgroundGeofencing.add(geofence);
-      let storedGeofences = await this.getGeofences();
-      if (storedGeofences.length) {
-        storedGeofences = storedGeofences.filter(
-          fence => fence.id !== geofence.id,
-        );
-      }
-      storedGeofences.unshift(geofence);
+      await RNBackgroundGeofencing.add(geofence.configuration);
+      const geofences = (await this.getGeofences()) || [];
+      geofences.unshift(geofence);
       await AsyncStorage.setItem(
         this.DB_STORED_GEOFENCE_KEY,
-        JSON.stringify(storedGeofences),
+        JSON.stringify(geofences),
       );
-      return storedGeofences;
+      console.log(
+        'geofence set and saved with configutation:\n',
+        geofence.configuration,
+      );
+      return geofences;
     } catch (error) {
-      console.log(error);
-      return [];
+      console.log(geofence.configuration);
+      throw error;
     }
   };
 
   getGeofences = async () => {
     try {
-      let storedGeofences = await AsyncStorage.getItem(
-        this.DB_STORED_GEOFENCE_KEY,
-      );
-      storedGeofences = storedGeofences ? JSON.parse(storedGeofences) : [];
-      return storedGeofences;
+      const geofences = await AsyncStorage.getItem(this.DB_STORED_GEOFENCE_KEY);
+      if (geofences) {
+        return JSON.parse(geofences);
+      } else {
+        return [];
+      }
     } catch (error) {
       console.log(error);
       return [];
@@ -78,36 +78,36 @@ export default class Repository {
     }
   };
 
-  addGeofenceEvent = async (geofenceId, event, data) => {
-    try {
-      const KEY = `${this.DB_STORED_GEOFENCE_EVENT_PREFIX}${geofenceId}`;
-      let storedGeofenceEvents = await this.getGeofenceEvents(geofenceId);
-      storedGeofenceEvents.unshift({event, data});
-      await AsyncStorage.setItem(KEY, JSON.stringify(storedGeofenceEvents));
-      console.log('Geofence event AsyncSaved: ', geofenceId);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // addGeofenceEvent = async (geofenceId, event, data) => {
+  //   try {
+  //     const KEY = `${this.DB_STORED_GEOFENCE_EVENT_PREFIX}${geofenceId}`;
+  //     let storedGeofenceEvents = await this.getGeofenceEvents(geofenceId);
+  //     storedGeofenceEvents.unshift({event, data});
+  //     await AsyncStorage.setItem(KEY, JSON.stringify(storedGeofenceEvents));
+  //     console.log('Geofence event AsyncSaved: ', geofenceId);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
-  removeGeofence = async geofenceId => {
-    try {
-      const KEY = `${this.DB_STORED_GEOFENCE_EVENT_PREFIX}${geofenceId}`;
-      let storedGeofences = await this.getGeofences();
-      storedGeofences = storedGeofences.filter(
-        fence => fence.id !== geofenceId,
-      );
-      await AsyncStorage.setItem(
-        this.DB_STORED_GEOFENCE_KEY,
-        JSON.stringify(storedGeofences),
-      );
-      console.log('removed Geofence from gen pop');
-      await AsyncStorage.removeItem(KEY);
-      console.log('removed Geofence events');
-      await RNBackgroundGeofencing.remove(geofenceId);
-      console.log('removed listener');
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // removeGeofence = async geofenceId => {
+  //   try {
+  //     const KEY = `${this.DB_STORED_GEOFENCE_EVENT_PREFIX}${geofenceId}`;
+  //     let storedGeofences = await this.getGeofences();
+  //     storedGeofences = storedGeofences.filter(
+  //       fence => fence.id !== geofenceId,
+  //     );
+  //     await AsyncStorage.setItem(
+  //       this.DB_STORED_GEOFENCE_KEY,
+  //       JSON.stringify(storedGeofences),
+  //     );
+  //     console.log('removed Geofence from gen pop');
+  //     await AsyncStorage.removeItem(KEY);
+  //     console.log('removed Geofence events');
+  //     await RNBackgroundGeofencing.remove(geofenceId);
+  //     console.log('removed listener');
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 }
