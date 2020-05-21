b6e0dc46ad0e6f05ba5e3c926127ef5aebb30a2e
Julius Kiano <jgkiano@gmail.com>
2020-05-21 09:07:20 +0300

    use jsTask to store geofences

--- example/index.js
+++ example/index.js
@@ -1,9 +1,21 @@
 import {AppRegistry} from 'react-native';
-import {configureWebhook} from 'react-native-background-geofencing';
+import {
+  configureWebhook,
+  configureJSTask,
+} from 'react-native-background-geofencing';
 import App from './App';
 import {name as appName} from './app.json';
 import secrets from './secrets.json';
+import GeofenceTask from './src/services/GeofenceTask';
 
 configureWebhook({url: secrets.webhook});
+
+configureJSTask({
+  notification: {
+    title: 'New geofence event',
+    text: 'Processing geofence event..',
+  },
+  task: GeofenceTask,
+});
 
 AppRegistry.registerComponent(appName, () => App);
