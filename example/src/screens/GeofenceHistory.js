deb28fc47e1b53fc088efa66c5e431f6a48ac2e0
Julius Kiano <jgkiano@gmail.com>
2020-05-22 08:51:53 +0300

    created render helper functions

--- example/src/screens/GeofenceHistory.js
+++ example/src/screens/GeofenceHistory.js
@@ -1,22 +1,46 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {FlatList} from 'react-native';
+import styled from 'styled-components';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 class GeofenceHistory extends React.Component {
+  events = [];
+
   constructor(props) {
     super(props);
     const {
       navigation,
       route: {params},
     } = props;
-    const {geofence} = params;
+    const {geofence, events} = params;
     const {address} = geofence;
     let title = address.title || 'Geofence History';
     title = title.length > 22 ? `${title.substring(0, 22)}...` : title;
     navigation.setOptions({title});
+    this.events = events;
   }
+
+  renderItem = () => {
+    return null;
+  };
+  renderList = () => {
+    console.log(this.events);
+    return (
+      <FlatList
+        data={this.events}
+        renderItem={({item}) => this.renderItem(item)}
+        keyExtractor={({id}) => id}
+      />
+    );
+  };
+
   render() {
-    return <Text>History me this!</Text>;
+    return <Container>{this.renderList()}</Container>;
   }
 }
+
+const Container = styled.View`
+  flex: 1;
+`;
 
 export default GeofenceHistory;
