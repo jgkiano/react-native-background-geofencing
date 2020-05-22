a5dc1251916cc43c775d63ddf5b8f849be366661
Julius Kiano <jgkiano@gmail.com>
2020-05-22 19:56:07 +0300

    created maps component to test

--- example/src/screens/GeofenceEventMap.js
+++ example/src/screens/GeofenceEventMap.js
@@ -1,6 +1,20 @@
 import React from 'react';
-import {Text} from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {Text, View} from 'react-native';
 
 export default function GeofenceEventMapScreen({route}) {
-  return <Text>Map me this</Text>;
+  return (
+    <View style={{flex: 1}}>
+      <MapView
+        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        style={{flex: 1}}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+      />
+    </View>
+  );
 }
