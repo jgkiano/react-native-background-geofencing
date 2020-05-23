2a60ddef3d28bf723e2e0a1f2cd571c232029c3d
Julius Kiano <jgkiano@gmail.com>
2020-05-23 12:29:35 +0300

    created GeofenceEventMapScreen

--- example/src/screens/GeofenceEventMap.js
+++ example/src/screens/GeofenceEventMap.js
@@ -1,20 +1,64 @@
 import React from 'react';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
-import {Text, View} from 'react-native';
+import styled from 'styled-components';
+import MapView, {PROVIDER_GOOGLE, Marker, Circle} from 'react-native-maps';
 
-export default function GeofenceEventMapScreen({route}) {
+const FAR_ZOOM_LEVEL = {
+  latitudeDelta: 0.02829186345218493,
+  longitudeDelta: 0.01591019332408905,
+};
+
+export default function GeofenceEventMapScreen({route: {params}, navigation}) {
+  const {
+    geofenceEvent,
+    geofence: {configuration, address},
+  } = params;
+  const geofenceAddress = {
+    latitude: configuration.lat,
+    longitude: configuration.lng,
+  };
+  const region = {
+    ...geofenceAddress,
+    ...FAR_ZOOM_LEVEL,
+  };
+  const triggerCoord = {
+    latitude: geofenceEvent.lat,
+    longitude: geofenceEvent.lng,
+  };
+  const mapStyles = {flex: 1};
   return (
-    <View style={{flex: 1}}>
-      <MapView
-        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-        style={{flex: 1}}
-        region={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0121,
-        }}
-      />
-    </View>
+    <Container>
+      <MapView provider={PROVIDER_GOOGLE} style={mapStyles} region={region}>
+        <Marker
+          title={address.title || 'Geofence Address'}
+          description="Where the geofence was created"
+          pinColor="#1565C0"
+          coordinate={triggerCoord}
+        />
+        <Marker
+          title="Triggering location"
+          description="Where the geofence event was triggered"
+          pinColor="#b71c1c"
+          coordinate={triggerCoord}
+        />
+        <Circle
+          center={geofenceAddress}
+          radius={configuration.radius}
+          strokeWidth={2}
+          strokeColor="rgba(21,101,192, 0.5)"
+          fillColor="rgba(21,101,192, 0.2)"
+        />
+        <Circle
+          center={triggerCoord}
+          radius={geofenceEvent.accuracy}
+          strokeWidth={2}
+          strokeColor="rgba(183,28,28, 0.5)"
+          fillColor="rgba(183,28,28, 0.2)"
+        />
+      </MapView>
+    </Container>
   );
 }
+
+const Container = styled.View`
+  flex: 1;
+`;
