0bc380e9312a42cf7b6c42aa6e88dd5afcdee44e
Julius Kiano <jgkiano@gmail.com>
2020-05-28 21:56:58 +0300

    init segment on index.js

--- example/index.js
+++ example/index.js
@@ -3,10 +3,13 @@ import {
   configureWebhook,
   configureJSTask,
 } from 'react-native-background-geofencing';
+import {initSegment} from './src/services/Segment';
 import App from './App';
 import {name as appName} from './app.json';
 import secrets from './secrets.json';
 import GeofenceTask from './src/services/GeofenceTask';
+
+initSegment();
 
 configureWebhook({url: secrets.webhook});
 
