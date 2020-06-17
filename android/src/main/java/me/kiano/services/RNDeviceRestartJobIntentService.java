d7c648f53939adf3b45058763fe2b03eda626539
Julius Kiano <jgkiano@gmail.com>
2020-06-17 10:44:46 +0300

    use static method for restarting geofences

--- android/src/main/java/me/kiano/services/RNDeviceRestartJobIntentService.java
+++ android/src/main/java/me/kiano/services/RNDeviceRestartJobIntentService.java
@@ -1,19 +1,17 @@
 package me.kiano.services;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Handler;
-import android.os.Looper;
+import android.content.IntentFilter;
+import android.location.LocationManager;
+import android.os.Build;
 import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.JobIntentService;
 
-import java.util.ArrayList;
-
-import me.kiano.database.RNGeofenceDB;
-import me.kiano.interfaces.RNGeofenceHandler;
 import me.kiano.models.RNGeofence;
 
 public class RNDeviceRestartJobIntentService extends JobIntentService {
@@ -27,23 +25,17 @@ public class RNDeviceRestartJobIntentService extends JobIntentService {
 
     @Override
     protected void onHandleWork(@NonNull Intent intent) {
-        RNGeofenceDB db = new RNGeofenceDB(getApplication());
-        ArrayList<RNGeofence> storedGeofences = db.getAllGeofences();
-        Log.v(TAG, "RNDeviceRestartJobIntentService work started: " + storedGeofences.size());
-
-        for (RNGeofence storedGeofence : storedGeofences) {
-            storedGeofence.start(false, new RNGeofenceHandler() {
-                @Override
-                public void onSuccess(final String geofenceId) {
-                    Log.v(TAG, "Geofence successfully reinitialised: " + geofenceId);
-                }
-
-                @Override
-                public void onError(final String geofenceId, Exception e) {
-                    Log.v(TAG, "Geofence FAILED reinitialisation: " + geofenceId);
-                    Log.e(TAG, e.getMessage());
-                }
-            });
+        if (RNGeofence.hasLocationPermission(getApplicationContext()) && RNGeofence.isLocationEnabled(getApplicationContext())) {
+            Log.v(TAG, "Starting to re register geofences");
+            RNGeofence.reRegisterStoredGeofences(getApplicationContext());
+        } else {
+            Log.v(TAG, "Starting location provider listener service..");
+            Intent service = new Intent(getApplicationContext(), RNLocationProviderChangeBroadcastListener.class);
+            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                getApplicationContext().startForegroundService(service);
+            } else {
+                getApplicationContext().startService(service);
+            }
         }
     }
 }
