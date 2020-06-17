ccbbd1f39f7cc2b04fdcc13bd13935fdcb399e0f
Julius Kiano <jgkiano@gmail.com>
2020-06-17 15:06:15 +0300

    remove unused imports

--- android/src/main/java/me/kiano/services/RNGeofenceWebhookWorker.java
+++ android/src/main/java/me/kiano/services/RNGeofenceWebhookWorker.java
@@ -12,7 +12,6 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.concurrent.TimeUnit;
 
