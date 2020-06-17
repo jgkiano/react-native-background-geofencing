a00f04c348930618f2af7a36c3c19c2052682c43
Julius Kiano <jgkiano@gmail.com>
2020-06-17 15:05:19 +0300

    cleaned up restart work

--- android/src/main/java/me/kiano/services/RNDeviceRestartJobIntentService.java
+++ android/src/main/java/me/kiano/services/RNDeviceRestartJobIntentService.java
@@ -1,13 +1,8 @@
 package me.kiano.services;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.location.LocationManager;
-import android.os.Build;
 import android.util.Log;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.JobIntentService;
@@ -25,17 +20,6 @@ public class RNDeviceRestartJobIntentService extends JobIntentService {
 
     @Override
     protected void onHandleWork(@NonNull Intent intent) {
-        if (RNGeofence.hasLocationPermission(getApplicationContext()) && RNGeofence.isLocationEnabled(getApplicationContext())) {
-            Log.v(TAG, "Starting to re register geofences");
-            RNGeofence.reRegisterStoredGeofences(getApplicationContext());
-        } else {
-            Log.v(TAG, "Starting location provider listener service..");
-            Intent service = new Intent(getApplicationContext(), RNLocationProviderChangeBroadcastListener.class);
-            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                getApplicationContext().startForegroundService(service);
-            } else {
-                getApplicationContext().startService(service);
-            }
-        }
+        RNGeofence.reRegisterStoredGeofences(getApplicationContext());
     }
 }
