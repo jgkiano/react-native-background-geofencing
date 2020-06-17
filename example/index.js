8df0b583e6d3a497417ced815f05f978d1d33370
Julius Kiano <jgkiano@gmail.com>
2020-06-17 15:34:16 +0300

    separated configuration

--- example/index.js
+++ example/index.js
@@ -1,31 +1,39 @@
 import {AppRegistry} from 'react-native';
-import {configure} from 'react-native-background-geofencing';
+import RNBackgroundGeofencing from 'react-native-background-geofencing';
 import App from './App';
 import {name as appName} from './app.json';
 import task from './src/services/task';
 import secrets from './secrets.json';
 
-configure({
-  notification: {
-    title: 'Starting set up',
-    text: 'This will only take a sec..',
-    importance: 3,
-    channelId: 'myChannelId',
-    channelName: 'My Channel Name',
-    channelDescription: 'My channel description',
-  },
-  webhook: {
-    url: secrets.webhook,
-    meta: {
-      lib: {
-        name: 'foo',
-        version: 'bar',
-      },
+RNBackgroundGeofencing.init();
+
+const notification = {
+  title: 'Starting set up',
+  text: 'This will only take a sec..',
+  importance: 3,
+  channelId: 'myChannelId',
+  channelName: 'My Channel Name',
+  channelDescription: 'My channel description',
+};
+
+const webhook = {
+  url: secrets.webhook,
+  meta: {
+    lib: {
+      name: 'foo',
+      version: 'bar',
     },
   },
-  jsTask: {
-    task,
-  },
+};
+
+const jsTask = {
+  task,
+};
+
+RNBackgroundGeofencing.configure({
+  notification,
+  webhook,
+  jsTask,
 });
 
 AppRegistry.registerComponent(appName, () => App);
