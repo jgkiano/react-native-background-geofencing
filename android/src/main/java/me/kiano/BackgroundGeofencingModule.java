7c5cf882557758fecfe00cd17974d424c5c820af
Julius Kiano <jgkiano@gmail.com>
2020-06-19 13:19:41 +0300

    remove schedule on add

--- android/src/main/java/me/kiano/BackgroundGeofencingModule.java
+++ android/src/main/java/me/kiano/BackgroundGeofencingModule.java
@@ -1,5 +1,7 @@
 package me.kiano;
 
+import android.util.Log;
+
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -8,12 +10,16 @@ import com.facebook.react.bridge.ReadableMap;
 
 import org.json.JSONException;
 
+import java.util.ArrayList;
+
+import me.kiano.database.RNGeofenceDB;
 import me.kiano.interfaces.RNGeofenceHandler;
 import me.kiano.models.RNGeofence;
 import me.kiano.models.RNGeofenceWebhookConfiguration;
 import me.kiano.models.RNNotification;
 
 public class BackgroundGeofencingModule extends ReactContextBaseJavaModule {
+    private String TAG = "BackgroundGeofencing";
 
     public BackgroundGeofencingModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -43,9 +49,6 @@ public class BackgroundGeofencingModule extends ReactContextBaseJavaModule {
             rnGeofence.start(true, rnGeofence.setInitialTriggers, new RNGeofenceHandler() {
                 @Override
                 public void onSuccess(String geofenceId) {
-                    if (rnGeofence.registerOnDeviceRestart) {
-                        RNGeofence.schedulePeriodicWork(getReactApplicationContext());
-                    }
                     promise.resolve(geofenceId);
                 }
                 @Override
@@ -114,6 +117,34 @@ public class BackgroundGeofencingModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void isLocationServicesEnabled(Promise promise) {
         promise.resolve(RNGeofence.isLocationServicesEnabled(getReactApplicationContext()));
+    }
+
+    @ReactMethod
+    public void init() {
+        if (RNGeofence.hasLocationPermission(getReactApplicationContext()) && RNGeofence.isLocationServicesEnabled(getReactApplicationContext())) {
+            final RNGeofenceDB db = new RNGeofenceDB(getReactApplicationContext());
+            ArrayList<RNGeofence> geofences = db.getAllErrorGeofences();
+            if (!geofences.isEmpty()) {
+                RNGeofence.restartGeofences(getReactApplicationContext(), geofences, new RNGeofenceHandler() {
+                    @Override
+                    public void onSuccess(String geofenceId) {
+                        Log.v(TAG, "Successfully re-registered geofence in init: " + geofenceId);
+                        db.removeErrorGeofence(geofenceId);
+                    }
+
+                    @Override
+                    public void onError(String geofenceId, Exception e) {
+                        Log.v(TAG, "Failed to re-register geofence in init will try later: " + geofenceId);
+                        db.saveErrorGeofence(geofenceId);
+                        RNGeofence.schedulePeriodicWork(getReactApplicationContext());
+                        e.printStackTrace();
+                    }
+                });
+            } else {
+                Log.v(TAG, "No bad geofences yet, cancelling any existing jobs");
+                RNGeofence.cancelPeriodicWork(getReactApplicationContext());
+            }
+        }
     }
 
 }
