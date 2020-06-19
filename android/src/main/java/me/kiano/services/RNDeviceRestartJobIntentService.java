493ee58e642db3419b79e541d80db3b441a57895
Julius Kiano <jgkiano@gmail.com>
2020-06-19 13:22:24 +0300

    start up to reset error geofences and register saved

--- android/src/main/java/me/kiano/services/RNDeviceRestartJobIntentService.java
+++ android/src/main/java/me/kiano/services/RNDeviceRestartJobIntentService.java
@@ -10,6 +10,7 @@ import androidx.core.app.JobIntentService;
 import java.util.ArrayList;
 
 import me.kiano.database.RNGeofenceDB;
+import me.kiano.interfaces.RNGeofenceHandler;
 import me.kiano.models.RNGeofence;
 
 public class RNDeviceRestartJobIntentService extends JobIntentService {
@@ -23,12 +24,40 @@ public class RNDeviceRestartJobIntentService extends JobIntentService {
 
     @Override
     protected void onHandleWork(@NonNull Intent intent) {
-        if (RNGeofence.isLocationServicesEnabled(getApplicationContext()) && RNGeofence.hasLocationPermission(getApplicationContext())) {
-            RNGeofenceDB db = new RNGeofenceDB(getApplicationContext());
-            ArrayList<RNGeofence> geofences = db.getAllRestartGeofences();
-            if (geofences.size() > 0) {
-                RNGeofence.schedulePeriodicWork(getApplicationContext());
+        // cancel any existing jobs
+        RNGeofence.cancelPeriodicWork(getApplicationContext());
+
+        // get db instance
+        final RNGeofenceDB db = new RNGeofenceDB(getApplicationContext());
+
+        // remove all error geofences
+        db.removeAllErrorGeofence();
+
+        // get all geofences with a restart policy
+        ArrayList<RNGeofence> geofences = db.getAllGeofences();
+        ArrayList<RNGeofence> restartGeofences = new ArrayList<>();
+        for (RNGeofence geofence: geofences) {
+            if (geofence.registerOnDeviceRestart) {
+                restartGeofences.add(geofence);
             }
+        }
+
+        // start geofences with restart policy, if failed to start add them to db to restart later
+        if (!restartGeofences.isEmpty()) {
+            RNGeofence.restartGeofences(getApplicationContext(), restartGeofences, new RNGeofenceHandler() {
+                @Override
+                public void onSuccess(String geofenceId) {
+                    Log.v(TAG, "Successfully registered geofence after device restart: " + geofenceId);
+                    db.removeErrorGeofence(geofenceId);
+                }
+
+                @Override
+                public void onError(String geofenceId, Exception e) {
+                    Log.v(TAG, "Failed geofence registration after device restart will try later: " + geofenceId);
+                    db.saveErrorGeofence(geofenceId);
+                    RNGeofence.schedulePeriodicWork(getApplicationContext());
+                }
+            });
         }
     }
 }
