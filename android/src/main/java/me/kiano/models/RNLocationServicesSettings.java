36518d35b7509a379eb3174a1245d797b3bb406e
Julius Kiano <jgkiano@gmail.com>
2020-06-29 16:54:05 +0300

    added static methods refactored class def

--- android/src/main/java/me/kiano/models/RNLocationServicesSettings.java
+++ android/src/main/java/me/kiano/models/RNLocationServicesSettings.java
@@ -1,14 +1,22 @@
 package me.kiano.models;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Handler;
+import android.provider.Settings;
+import android.text.TextUtils;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 
 import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.modules.core.PermissionListener;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.LocationRequest;
@@ -20,51 +28,85 @@ import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
-import me.kiano.interfaces.RNLocationServicesRequestHandler;
+import me.kiano.interfaces.RNRequestHandler;
 
-public class RNLocationServicesSettings {
+public class RNLocationServicesSettings implements PermissionListener {
 
     private String TAG = "RNLocationServicesSettings";
     private Activity activity;
     private static final int REQUEST_SETTINGS_SINGLE_UPDATE = 11403;
+    private static final int REQUEST_LOCATION_PERMISSION = 10007;
     private LocationSettingsRequest locationSettingsRequest = buildLocationSettingsRequest();
     private SettingsClient settingsClient;
-    final Handler handler = new Handler();
+    private Context context;
+    private RNRequestHandler rnRequestHandler;
 
-    public RNLocationServicesSettings(Activity activity, ReactApplicationContext context, final RNLocationServicesRequestHandler locationServicesRequestHandler) {
+    public RNLocationServicesSettings(Activity activity, final ReactApplicationContext context) {
+
         this.activity = activity;
+        this.context = context;
         this.settingsClient = LocationServices.getSettingsClient(context);
+
         ActivityEventListener activityEventListener = new ActivityEventListener() {
             boolean invokedHandler = false;
+            Handler handler = new Handler();
+
             @Override
             public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
                 Log.v(TAG, "requestCode: " + requestCode + ", resultCode: " + resultCode);
-                if (requestCode == REQUEST_SETTINGS_SINGLE_UPDATE) {
-                    if (!invokedHandler) {
+                if (invokedHandler) {
+                    return;
+                }
+                invokedHandler = true;
+                switch (requestCode) {
+                    case REQUEST_SETTINGS_SINGLE_UPDATE:
                         if (resultCode == Activity.RESULT_OK) {
                             handler.postDelayed(new Runnable() {
                                 @Override
                                 public void run() {
-                                    locationServicesRequestHandler.onSuccess();
+                                    rnRequestHandler.onSuccess();
                                 }
-                            }, 5000);
+                            }, 3000);
                             Log.v(TAG, "Location services enabled: " + resultCode);
                         } else {
-                            locationServicesRequestHandler.onError();
+                            rnRequestHandler.onError();
                             Log.v(TAG, "Location services unavailable: " + resultCode);
                         }
-                        invokedHandler = true;
-                    }
-
+                        break;
+                    default:
+                        break;
                 }
             }
-
             @Override
             public void onNewIntent(Intent intent) {
 
             }
         };
         context.addActivityEventListener(activityEventListener);
+    }
+
+    public static boolean hasLocationPermission(Context context) {
+        int permission = ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION);
+        return permission == PackageManager.PERMISSION_GRANTED;
+    }
+
+
+    public static boolean isLocationServicesEnabled(Context context) {
+        int locationMode = 0;
+        String locationProviders;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            try {
+                locationMode = Settings.Secure.getInt(context.getContentResolver(), Settings.Secure.LOCATION_MODE);
+            } catch (Settings.SettingNotFoundException e) {
+                e.printStackTrace();
+                return false;
+            }
+            return locationMode != Settings.Secure.LOCATION_MODE_OFF;
+        } else {
+            locationProviders = Settings.Secure.getString(context.getContentResolver(),
+                    Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+            return !TextUtils.isEmpty(locationProviders);
+        }
     }
 
     private LocationSettingsRequest buildLocationSettingsRequest() {
@@ -95,7 +137,9 @@ public class RNLocationServicesSettings {
         }
     }
 
-    public void showLocationServicesRequestDialog() {
+    public void requestEnableLocationServices(RNRequestHandler handler) {
+        this.rnRequestHandler = handler;
+
         settingsClient.checkLocationSettings(locationSettingsRequest).addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
             @Override
             public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
@@ -104,4 +148,8 @@ public class RNLocationServicesSettings {
         });
     }
 
+    @Override
+    public boolean onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        return false;
+    }
 }
