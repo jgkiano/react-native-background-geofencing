391f7913bf07f38b4b8bff2c11846f03ece68478
Julius Kiano <jgkiano@gmail.com>
2020-07-04 15:31:01 +0300

    created type definitions for methods

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,18 @@
 export interface RNBackgroundGeofencing {
   configureWebhook(webhook: RNBackgroundGeofencingWebhook): Promise<boolean>;
+  configureNotification(notification: RNBackgroundGeofencingNotification): Promise<boolean>;
+  add(geofence: RNBackgroundGeofence): Promise<string>;
+  isLocationPermissionGranted(): Promise<boolean>;
+  isLocationServicesEnabled(): Promise<boolean>;
+  // start android specific methods
+  isGooglePlayServicesAvailable(): Promise<boolean>;
+  requestEnableGooglePlayServices(): Promise<boolean>;
+  requestEnableLocationServices(): Promise<boolean>;
+  // end android specific methods
+  openLocationServicesSettings(): void;
+  remove(geofenceId: string): Promise<string>;
+  init(): void;
+  restart(): void;
 }
 
 export interface RNBackgroundGeofencingWebhook {
@@ -13,7 +26,9 @@ export interface RNBackgroundGeofencingWebhook {
   };
 }
 
-type ERROR_CODES = 'invalid_platform' | 'geofence_exception';
+export type ERROR_CODES = 'invalid_platform' | 'geofence_exception';
+
+export type RNGeofenceTransitionType = 'enter' | 'exit' | 'dwell';
 
 export class RNBackgroundGeofencingException extends Error {
   code: string;
@@ -23,4 +38,56 @@ export class RNBackgroundGeofencingException extends Error {
     this.message = error.message;
     this.code = error.code;
   }
+}
+
+export interface RNBackgroundGeofencingAndroidNotification {
+  importance: number;
+  channelId: string;
+  channelName: string;
+  channelDescription: string;
+}
+
+export interface RNGeofenceTransitionData {
+  accuracy: number;
+  altitude: number;
+  bearing: number;
+  time: number;
+  provider: string;
+  lat: number;
+  lng: number;
+  geofenceIds: Array<string>;
+}
+
+export interface RNBackgroundGeofencingNotification {
+  title: string;
+  text: string;
+  android: RNBackgroundGeofencingAndroidNotification;
+}
+
+export interface RNBackgroundGeofencingTransitionEvent {
+  transition: RNGeofenceTransitionType;
+  data: RNGeofenceTransitionData;
+}
+
+export interface RNBackgroundGeofencingJSTask {
+  task: (transition: RNBackgroundGeofencingTransitionEvent) => Promise<any>;
+  notification: RNBackgroundGeofencingNotification;
+}
+
+export interface RNBackgroundGeofencingConfiguration {
+  webhook?: RNBackgroundGeofencingWebhook;
+  jsTask?: RNBackgroundGeofencingJSTask;
+}
+
+export interface RNBackgroundGeofence {
+  id: string;
+  lat: number;
+  lng: number;
+  radius: number;
+  expiresIn: string | number;
+  loiteringDelay: string | number;
+  notificationResponsiveness: string | number;
+  transitionTypes: Array<RNGeofenceTransitionType>;
+  initialTriggerTransitionTypes: Array<RNGeofenceTransitionType>;
+  registerOnDeviceRestart: boolean;
 }
