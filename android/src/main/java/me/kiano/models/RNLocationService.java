dbdf4952c4862aff22bf5231183278d6b998af6d
Julius Kiano <jgkiano@gmail.com>
2020-07-12 14:37:10 +0300

    created isLocationServicesEnabled method

--- android/src/main/java/me/kiano/models/RNLocationService.java
+++ android/src/main/java/me/kiano/models/RNLocationService.java
@@ -2,8 +2,10 @@ package me.kiano.models;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.text.TextUtils;
 import android.util.Log;
 
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -15,5 +17,23 @@ public class RNLocationService {
         Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
         context.startActivityForResult(intent, Constant.OPEN_LOCATION_SERVICES_SETTINGS_REQUEST_CODE, new Bundle());
         Log.v(TAG, "launched location services successfully");
+    }
+
+    public static boolean isLocationServicesEnabled(Context context) {
+        int locationMode = 0;
+        String locationProviders;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            try {
+                locationMode = Settings.Secure.getInt(context.getContentResolver(), Settings.Secure.LOCATION_MODE);
+            } catch (Settings.SettingNotFoundException e) {
+                e.printStackTrace();
+                return false;
+            }
+            return locationMode != Settings.Secure.LOCATION_MODE_OFF;
+        } else {
+            locationProviders = Settings.Secure.getString(context.getContentResolver(),
+                    Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+            return !TextUtils.isEmpty(locationProviders);
+        }
     }
 }
