23e7efbf6801dd0518a7e892a742546b191c05be
Julius Kiano <jgkiano@gmail.com>
2020-07-12 16:00:49 +0300

    added constant values

--- android/src/main/java/me/kiano/models/Constant.java
+++ android/src/main/java/me/kiano/models/Constant.java
@@ -2,4 +2,6 @@ package me.kiano.models;
 
 public class Constant {
     public static int OPEN_LOCATION_SERVICES_SETTINGS_REQUEST_CODE = 1;
+    public static int ENABLE_LOCATION_SERVICES_REQUEST_CODE = 2;
+    public static int SERVICE_WAIT_DELAY = 3000;
 }
