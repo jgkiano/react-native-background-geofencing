feca00207682c16d521369eacc54a0d11c77d47c
Julius Kiano <jgkiano@gmail.com>
2020-07-13 10:32:39 +0300

    centralised constant values

--- android/src/main/java/me/kiano/models/Constant.java
+++ android/src/main/java/me/kiano/models/Constant.java
@@ -1,8 +1,18 @@
 package me.kiano.models;
 
 public class Constant {
+    // request codes
     public static int OPEN_LOCATION_SERVICES_SETTINGS_REQUEST_CODE = 1;
     public static int ENABLE_LOCATION_SERVICES_REQUEST_CODE = 2;
     public static int ENABLE_GOOGLE_PLAY_SERVICES_REQUEST_CODE = 3;
+
+    // timers and delays
     public static int SERVICE_WAIT_DELAY = 3000;
+    public static int DEFAULT_WEBHOOK_TIMEOUT = 15000;
+
+    // db
+    public static String RN_DB_NAME = "RNBackgroundGeofencingDB";
+    public static String RN_GEOFENCE_KEY_PREFIX = "RNGeofenceDB:v1:";
+    public static String RN_WEBHOOK_CONFIG_KEY = "RNWebhookDB:v1:configuration";
+    public static String RN_NOTIFICATION_CONFIG_KEY = "RNNotificationDB:v1:configuration";
 }
