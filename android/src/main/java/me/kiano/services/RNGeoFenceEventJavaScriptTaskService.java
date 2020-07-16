2becc448b27dfce223dca2214bf5926791998638
Julius Kiano <jgkiano@gmail.com>
2020-07-16 19:36:04 +0300

    cleaned up service start requirements

--- android/src/main/java/me/kiano/services/RNGeoFenceEventJavaScriptTaskService.java
+++ android/src/main/java/me/kiano/services/RNGeoFenceEventJavaScriptTaskService.java
@@ -1,56 +1,38 @@
 package me.kiano.services;
 
-import android.app.ActivityManager;
 import android.app.Notification;
-import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-
-import androidx.core.app.NotificationCompat;
 
 import com.facebook.react.HeadlessJsTaskService;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.jstasks.HeadlessJsTaskConfig;
 
-import java.util.List;
-
 import javax.annotation.Nullable;
 
 import me.kiano.database.RNGeofenceDB;
+import me.kiano.models.Constant;
+import me.kiano.models.RNHelper;
 import me.kiano.models.RNNotification;
 
 public class RNGeoFenceEventJavaScriptTaskService extends HeadlessJsTaskService {
 
-    private static final String CHANNEL_ID = "RNBackgroundGeofencing";
-
     @Override
     public void onCreate() {
         super.onCreate();
-        RNGeofenceDB db = new RNGeofenceDB(getApplicationContext());
-        RNNotification rnNotification = null;
-        try {
-            rnNotification = db.getNotification();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        if (!isAppOnForeground(getApplicationContext()) && android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && rnNotification != null) {
+        // get the current sdk version
+        int sdk = android.os.Build.VERSION.SDK_INT;
 
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "RNBackgroundGeofencing", importance);
-            channel.setDescription("Background geofencing service");
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-
-            Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
-                    .setContentTitle(rnNotification.getTitle())
-                    .setContentText(rnNotification.getText())
-                    .setSmallIcon(getApplicationContext().getApplicationInfo().icon)
-                    .build();
-
-            startForeground(1, notification);
+        if (!RNHelper.isAppOnForeground(getApplicationContext()) && sdk >= Build.VERSION_CODES.O) {
+            RNGeofenceDB db = new RNGeofenceDB(getApplicationContext());
+            RNNotification rnNotification = db.getNotification();
+            if (rnNotification != null) {
+                NotificationManager notificationManager = getSystemService(NotificationManager.class);
+                Notification notification = rnNotification.getNotification(notificationManager, getApplicationContext());
+                startForeground(Constant.RN_JS_FOREGROUND_SERVICE_ID, notification);
+            }
         }
     }
 
@@ -59,30 +41,12 @@ public class RNGeoFenceEventJavaScriptTaskService extends HeadlessJsTaskService 
         Bundle extras = intent.getExtras();
         if (extras != null) {
             return new HeadlessJsTaskConfig(
-                    "OnGeoFenceEventJavaScript",
+                    Constant.RN_JS_FOREGROUND_SERVICE_TASK_KEY,
                     Arguments.fromBundle(extras),
-                    30000, // timeout for the task
+                    Constant.RN_JS_FOREGROUND_SERVICE_TIMEOUT, // timeout for the task
                     true // optional: defines whether or not  the task is allowed in foreground. Default is false
             );
         }
         return null;
-    }
-
-    private boolean isAppOnForeground(Context context) {
-        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-        List<ActivityManager.RunningAppProcessInfo> appProcesses =
-                activityManager.getRunningAppProcesses();
-        if (appProcesses == null) {
-            return false;
-        }
-        final String packageName = context.getPackageName();
-        for (ActivityManager.RunningAppProcessInfo appProcess : appProcesses) {
-            if (appProcess.importance ==
-                    ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND &&
-                    appProcess.processName.equals(packageName)) {
-                return true;
-            }
-        }
-        return false;
     }
 }
