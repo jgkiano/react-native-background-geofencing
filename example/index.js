20337b4a543a0f1e08a5ce6b653cb7311b9a38b7
Julius Kiano <jgkiano@gmail.com>
2020-07-16 19:37:59 +0300

    use configure method

--- example/index.js
+++ example/index.js
@@ -1,10 +1,21 @@
 import {AppRegistry} from 'react-native';
-import {} from 'react-native-background-geofencing';
+import RNBackgroundGeofencing from 'react-native-background-geofencing';
 import {initSegment} from './src/services/Segment';
 import App from './App';
 import {name as appName} from './app.json';
+import task from './src/services/task';
 // import secrets from './secrets.json';
 
 initSegment();
+
+RNBackgroundGeofencing.configure({
+  jsTask: {
+    task,
+    notification: {
+      title: 'Work work',
+      text: 'Get that money',
+    },
+  },
+});
 
 AppRegistry.registerComponent(appName, () => App);
