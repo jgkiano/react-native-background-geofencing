2e3537e99c7cfe2e69befac2ac06a3dbaaec8929
Julius Kiano <jgkiano@gmail.com>
2020-07-16 19:36:43 +0300

    removed unused methods, refactored to levarge classes

--- android/src/main/java/me/kiano/services/RNGeofenceTransitionsJobIntentService.java
+++ android/src/main/java/me/kiano/services/RNGeofenceTransitionsJobIntentService.java
@@ -1,7 +1,6 @@
 package me.kiano.services;
 
 import android.app.ActivityManager;
-import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -15,6 +14,7 @@ import androidx.annotation.NonNull;
 import androidx.core.app.JobIntentService;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
+import androidx.work.BackoffPolicy;
 import androidx.work.Constraints;
 import androidx.work.Data;
 import androidx.work.NetworkType;
@@ -22,131 +22,99 @@ import androidx.work.OneTimeWorkRequest;
 import androidx.work.WorkManager;
 
 import com.facebook.react.HeadlessJsTaskService;
+import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingEvent;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import me.kiano.database.RNGeofenceDB;
-import me.kiano.models.RNGeofenceData;
-
-import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import me.kiano.models.Constant;
+import me.kiano.models.RNGeofence;
+import me.kiano.models.RNGeofenceOrigin;
+import me.kiano.models.RNGeofenceTransition;
+import me.kiano.models.RNGeofenceWebhook;
+import me.kiano.models.RNHelper;
+import me.kiano.models.RNNotification;
 
 public class RNGeofenceTransitionsJobIntentService extends JobIntentService {
 
     public static final String TAG = "RNBackgroundGeofencing";
 
     public static void enqueueWork(Context context, Intent work) {
-        Log.v(TAG, "GeofenceTransitionsJobIntentService called");
-        enqueueWork(context, RNGeofenceTransitionsJobIntentService.class, 456, work);
+        enqueueWork(context, RNGeofenceTransitionsJobIntentService.class, Constant.RN_GEOFENCE_TRANSITION_JOB_ID, work);
+        Log.v(TAG, "Geofence work successfully enqueued");
     }
 
     @Override
     protected void onHandleWork(@NonNull Intent intent) {
-        Log.v(TAG, "GeofenceTransitionsJobIntentService work started");
+        Log.v(TAG, "Geofence work started..");
         GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
-        RNGeofenceData rnGeofenceData = new RNGeofenceData(geofencingEvent);
-        RNGeofenceDB rnGeofenceDB = new RNGeofenceDB(getApplicationContext());
 
-        if (rnGeofenceDB.hasWebhookConfiguration()) {
+        if (geofencingEvent.hasError()) {
+            List<Geofence> failedGeofences = geofencingEvent.getTriggeringGeofences();
+            for (Geofence failedGeofence: failedGeofences) {
+                RNGeofence.setFailing(failedGeofence.getRequestId(), true, getApplicationContext());
+            }
+            RNGeofence.schedulePeriodicWork(getApplicationContext());
+            return;
+        }
+
+        RNGeofenceDB db = new RNGeofenceDB(getApplicationContext());
+        RNGeofenceTransition rnGeofenceTransition = new RNGeofenceTransition(geofencingEvent);
+        RNGeofenceWebhook rnGeofenceWebhook = db.getWebhookConfiguration();
+        RNNotification rnNotification = db.getNotification();
+
+        if (rnGeofenceWebhook != null) {
+            scheduleOneTimeUploadWork(rnGeofenceWebhook, rnGeofenceTransition);
+        }
+
+        if (rnNotification != null && rnGeofenceTransition.getTriggeringGeofenceOrigin(getApplicationContext()).equals(RNGeofenceOrigin.RN)) {
+            startHeadlessJavaScriptTask(rnGeofenceTransition, rnNotification);
+        }
+    }
+
+    private void startHeadlessJavaScriptTask(RNGeofenceTransition rnGeofenceTransition, RNNotification rnNotification) {
+        try {
+            Intent intent = new Intent(getApplicationContext(), RNGeoFenceEventJavaScriptTaskService.class);
+            Bundle bundle = new Bundle();
+            bundle.putString(Constant.RN_HEADLESS_JS_TRANSITION_EVENT, rnGeofenceTransition.toJSON());
+            intent.putExtras(bundle);
+            RNHelper.startService(intent, getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void scheduleOneTimeUploadWork(RNGeofenceWebhook webhook, RNGeofenceTransition transition) {
+        try {
             Constraints constraints = new Constraints.Builder()
                     .setRequiredNetworkType(NetworkType.CONNECTED)
                     .build();
 
             Data rnGeofenceWorkData = new Data.Builder()
-                    .putString("event", rnGeofenceData.getEventName())
-                    .putString("data", rnGeofenceData.getEventData())
+                    .putString(Constant.RN_UPLOAD_WORK_GEOFENCE_TRANSITION, transition.toJSON())
+                    .putString(Constant.RN_UPLOAD_WORK_WEBHOOK_CONFIG, webhook.toJSON())
                     .build();
 
             OneTimeWorkRequest uploadWorkRequest = new OneTimeWorkRequest.Builder(RNGeofenceWebhookWorker.class)
                     .setConstraints(constraints)
                     .setInputData(rnGeofenceWorkData)
-                    .addTag("RNGeofenceWork")
+                    .addTag(Constant.RN_ONE_TIME_WORK_TAG)
                     .setInitialDelay(1, TimeUnit.MINUTES)
+                    .setBackoffCriteria(
+                            BackoffPolicy.LINEAR,
+                            Constant.RN_UPLOAD_WORK_BACK_OFF_DELAY,
+                            Constant.RN_UPLOAD_WORK_BACK_OFF_DELAY_TIME_UNIT
+                    )
                     .build();
 
             WorkManager.getInstance(getApplicationContext()).enqueue(uploadWorkRequest);
 
-            Log.v(TAG, "Geofence work request queued up");
-        }
-
-        if (rnGeofenceDB.hasNotificationConfiguration()) {
-            Intent service = new Intent(getApplicationContext(), RNGeoFenceEventJavaScriptTaskService.class);
-            Bundle bundle = new Bundle();
-            bundle.putString("event", rnGeofenceData.getEventName());
-            bundle.putString("data", rnGeofenceData.getEventData());
-            Log.v(TAG, "Geofence transition: " + rnGeofenceData.getEventName());
-            Log.v(TAG, rnGeofenceData.getEventData());
-            service.putExtras(bundle);
-            if (!isAppOnForeground(getApplicationContext()) && android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                getApplicationContext().startForegroundService(service);
-            } else {
-                getApplicationContext().startService(service);
-            }
-            HeadlessJsTaskService.acquireWakeLockNow(getApplicationContext());
-        }
-
-        showEventNotification();
-    }
-
-    private void showEventNotification() {
-        Log.v(TAG, "Configuring notification..");
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        String CHANNEL_ID = "RNBackgroundGeofencingEventNotification";
-        String CHANNEL_NAME = "Geofencing Events Notification";
-        String CHANNEL_DESCRIPTION = "Get notified when geofence events occurs";
-        int NOTIFICATION_ID = 2;
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, importance);
-            channel.setDescription(CHANNEL_DESCRIPTION);
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
-        Intent intent = new Intent(this, getMainActivityClass());
-        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), NOTIFICATION_ID, intent,
-                PendingIntent.FLAG_UPDATE_CURRENT);
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(getApplicationContext().getApplicationInfo().icon)
-                .setContentTitle("You have a new geofence event to review")
-                .setContentText("Tap to review")
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(true)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-        notificationManager.notify(NOTIFICATION_ID, builder.build());
-        Log.v(TAG, "Sent notification 🤞");
-    }
-
-    public Class getMainActivityClass() {
-        String packageName = getApplicationContext().getPackageName();
-        Intent launchIntent = getApplicationContext().getPackageManager().getLaunchIntentForPackage(packageName);
-        String className = launchIntent.getComponent().getClassName();
-        try {
-            return Class.forName(className);
-        } catch (ClassNotFoundException e) {
+            Log.v(TAG, "Geofence one-time work request queued up");
+        } catch (Exception e) {
             e.printStackTrace();
-            return null;
         }
-    }
-
-    private boolean isAppOnForeground(Context context) {
-        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-        List<ActivityManager.RunningAppProcessInfo> appProcesses =
-                activityManager.getRunningAppProcesses();
-        if (appProcesses == null) {
-            return false;
-        }
-        final String packageName = context.getPackageName();
-        for (ActivityManager.RunningAppProcessInfo appProcess : appProcesses) {
-            if (appProcess.importance ==
-                    ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND &&
-                    appProcess.processName.equals(packageName)) {
-                return true;
-            }
-        }
-        return false;
     }
 }
