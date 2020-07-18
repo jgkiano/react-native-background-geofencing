83825e14942975736d79a8816ca93480de72f959
Julius Kiano <jgkiano@gmail.com>
2020-07-18 14:29:54 +0300

    rennamed to generic RNAndroidModule

--- android/src/main/java/me/kiano/RNAndroidModule.java
+++ android/src/main/java/me/kiano/RNAndroidModule.java
@@ -3,7 +3,7 @@ package me.kiano;
 import android.content.Context;
 import android.widget.Toast;
 
-public class OkVerify {
+public class RNAndroidModule {
     public static void ping(Context context) {
         Toast.makeText(context, "PONG", Toast.LENGTH_LONG).show();
     }
